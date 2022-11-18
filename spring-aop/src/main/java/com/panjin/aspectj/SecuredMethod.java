d9ba0fa857c859586ee9f9d2e4c132bda4cda914
pjslina <pjstart@126.com>
2022-11-18 23:49:20 +0800

    Annotating Aspects

---
+++ spring-aop/src/main/java/com/panjin/aspectj/SecuredMethod.java
@@ -0,0 +1,26 @@
+package com.panjin.aspectj;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author laiye
+ */
+public class SecuredMethod {
+    private static final Logger logger = LoggerFactory.getLogger(SecuredMethod.class);
+
+    @Secured(isLocked = true)
+    public void lockedMethod() throws Exception {
+        logger.info("lockedMethod");
+    }
+
+    @Secured(isLocked = false)
+    public void unlockedMethod() {
+        logger.info("unlockedMethod");
+    }
+
+    public static void main(String[] args) throws Exception {
+        SecuredMethod sv = new SecuredMethod();
+        sv.lockedMethod();
+    }
+}
