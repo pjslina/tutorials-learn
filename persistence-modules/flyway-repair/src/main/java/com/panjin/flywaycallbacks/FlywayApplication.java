f0d55c7ee0ac1ee2f1d88d20ec0317a86f52b37b
panjin <panjin@laiye.com>
2023-02-16 11:24:05 +0800

    add flyway repair module case

---
+++ persistence-modules/flyway-repair/src/main/java/com/panjin/flywaycallbacks/FlywayApplication.java
@@ -0,0 +1,16 @@
+package com.panjin.flywaycallbacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author panjin
+ */
+@SpringBootApplication
+public class FlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayApplication.class, args);
+    }
+
+}
