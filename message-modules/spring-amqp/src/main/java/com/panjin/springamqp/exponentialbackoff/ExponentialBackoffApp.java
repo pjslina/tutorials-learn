5ae50caf7ce3e15572b14b488c03b84b4386cbff
panjin <panjin@laiye.com>
2023-09-13 10:58:45 +0800

    rabbitmq

---
+++ message-modules/spring-amqp/src/main/java/com/panjin/springamqp/exponentialbackoff/ExponentialBackoffApp.java
@@ -0,0 +1,11 @@
+package com.panjin.springamqp.exponentialbackoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExponentialBackoffApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ExponentialBackoffApp.class, args);
+    }
+}
